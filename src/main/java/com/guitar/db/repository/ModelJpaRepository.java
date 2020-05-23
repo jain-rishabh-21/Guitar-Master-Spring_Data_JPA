d5af2311cbc9ceaf93554ee67e021ad4a79f7ed2
kackerzzz <rishabhee21@gmail.com>
2020-05-23 07:13:27 +0530

    Adding @Query Annotation to write a JPQA

--- src/main/java/com/guitar/db/repository/ModelJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ModelJpaRepository.java
@@ -2,7 +2,10 @@ package com.guitar.db.repository;
 
 import com.guitar.db.model.Manufacturer;
 import com.guitar.db.model.Model;
+import org.springframework.data.annotation.QueryAnnotation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -13,4 +16,9 @@ public interface ModelJpaRepository extends JpaRepository<Model, Long> {
     List<Model> findByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal low, BigDecimal high);
 
     List<Model> findByModelTypeNameIn(List<String> types);
+
+    @Query("select m from Model m where m.price >= :lowest and m.price <= :highest and m.woodType like :wood")
+    List<Model> queryByPriceRangeAAndWoodType(@Param("lowest") BigDecimal lowest,
+                                              @Param("highest") BigDecimal highest,
+                                              @Param("wood") String wood);
 }
